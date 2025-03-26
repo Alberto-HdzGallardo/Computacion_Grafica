b51008243e64368c89e0d2acd2b697ece7a7c3eb
Alberto J. Hdz. Gallardo <54250168+Alberto-HdzGallardo@users.noreply.github.com>
2025-03-26 10:57:06 -0600

    Practica 8
    Comienzo de la práctica

--- ConfigInicial/Iluminacion.cpp
+++ ConfigInicial/Iluminacion.cpp
@@ -51,6 +51,7 @@ GLfloat lastFrame = 0.0f;
 float rot = 0.0f;
 bool activanim = false;
 
+
 int main()
 {
     // Init GLFW
@@ -108,6 +109,15 @@ int main()
 
     // Load models
     Model red_dog((char*)"Models/RedDog.obj");
+    Model farm((char*)"Models/Farm_2.obj");
+    Model terrain((char*)"Models/Low Poly Farm Asset/terrain_sm.obj");
+    Model corns_sm((char*)"Models/Low Poly Farm Asset/corns_sm.obj");
+    Model hay((char*)"Models/Low Poly Farm Asset/hay.obj");
+    Model tree((char*)"Models/Low Poly Farm Asset/tree.obj");
+    Model stone_path_lowpoly((char*)"Models/Low Poly Farm Asset/stone_path_lowpoly.obj");
+    Model barrel((char*)"Models/Low Poly Farm Asset/barrel.obj");
+    Model barn((char*)"Models/Low Poly Farm Asset/barn.obj");
+    Model sol((char*)"Models/Sol/13913_Sun_v2_l3.obj");
     glm::mat4 projection = glm::perspective(camera.GetZoom(), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
 
     float vertices[] = {
@@ -152,6 +162,8 @@ int main()
          0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
         -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+
+       
     };
 
     // First, set the container's VAO (and VBO)
@@ -246,6 +258,147 @@ int main()
         glBindVertexArray(VAO);
         red_dog.Draw(lightingShader);
        
+        // Draw the loaded model Dog
+        
+        glm::mat4 doggy(1);
+        doggy = glm::translate(doggy, glm::vec3(0.0f, 2.0f, 0.0f));
+        model = doggy;
+        doggy = glm::rotate(model, glm::radians(-90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        doggy = glm::translate(doggy, glm::vec3(5.0f, 0.0f, 5.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(doggy));
+        red_dog.Draw(lightingShader);
+
+
+        //Terreno base
+        model = glm::translate(model, glm::vec3(-8.0f, -0.4f, 0.0f));
+        model = glm::scale(model, glm::vec3(2.0f, 2.0f, 2.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        terrain.Draw(lightingShader);
+
+        //Edificio principal
+        model = glm::translate(model, glm::vec3(-2.0f, 0.0f, -3.0f));
+        model = glm::scale(model, glm::vec3(0.02f, 0.02f, 0.02f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        farm.Draw(lightingShader);
+
+        //Maiz 1
+        model = glm::translate(model, glm::vec3(1.0f, 0.0f, 180.0f));
+        model = glm::scale(model, glm::vec3(9.0f, 18.0f, 9.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        corns_sm.Draw(lightingShader);
+
+        //Maiz 2
+        //NO se necesita transformacion de escala ya que se mantiene igual al modelo Maiz 1
+        //Debido a que comparten el mismo modelo se genera en la misma posicion por lo que se traslada 6 unidades en el eje Z a partir de la posicion de Maiz 1
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 6.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        corns_sm.Draw(lightingShader);
+
+        //Maiz 3
+        //De la misma forma que Maiz 2, comparten el mismo modelo y se trasladan 3 unidades en el eje X a partir de la posicion de Maiz 2
+        model = glm::translate(model, glm::vec3(3.0f, 0.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        corns_sm.Draw(lightingShader);
+
+        //Maiz 4
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, -6.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        corns_sm.Draw(lightingShader);
+
+        //Heno 1
+        glm::mat4 model2(1);
+        model2 = glm::translate(model2, glm::vec3(-18.0f, 1.7f, -9.0f));
+        model2 = glm::scale(model2, glm::vec3(0.5f, 0.5f, 0.5f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model2));
+        hay.Draw(lightingShader);
+
+        //Heno 2
+        //Mismo concepto que los maices, se modelan los demas Henos a partir de la posicion y escala del modelo original Heno 1
+        model2 = glm::translate(model2, glm::vec3(0.0f, 0.0f, -2.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model2));
+        hay.Draw(lightingShader);
+
+        //Heno 3
+        model2 = glm::translate(model2, glm::vec3(2.0f, 0.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model2));
+        hay.Draw(lightingShader);
+
+        //Heno 4
+        model2 = glm::translate(model2, glm::vec3(0.0f, 0.0f, 2.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model2));
+        hay.Draw(lightingShader);
+
+        //Arbol 1
+        model = glm::translate(model, glm::vec3(-38.0f, 0.0f, -58.0f));
+        model = glm::scale(model, glm::vec3(1.5f, 1.5f, 1.5f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        tree.Draw(lightingShader);
+        model2 = model;
+
+        //Arbol 2
+        model = glm::translate(model, glm::vec3(58.0f, 0.0f, 0.0f));
+        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        tree.Draw(lightingShader);
+
+        //Arbol 3
+        model2 = glm::translate(model2, glm::vec3(-8.0f, 0.0f, 4.0f));
+        model2 = glm::rotate(model2, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model2));
+        tree.Draw(lightingShader);
+
+        //Arbol 4
+        model2 = glm::translate(model2, glm::vec3(-8.0f, 0.0f, 3.0f));
+        model2 = glm::rotate(model2, glm::radians(135.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model2));
+        tree.Draw(lightingShader);
+
+        //Camino 1
+        //Se crea otra matriz para el camino para que sea mas sencillo de manipular
+        glm::mat4 camino(1);
+        camino = glm::translate(camino, glm::vec3(-16.7f, 1.6f, -5.7f));
+        glm::mat4 barril(1);
+        barril = camino;
+        camino = glm::rotate(camino, glm::radians(-90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(camino));
+        stone_path_lowpoly.Draw(lightingShader);
+
+        //Camino 2
+        camino = glm::translate(camino, glm::vec3(0.0f, 0.0f, 4.2f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(camino));
+        stone_path_lowpoly.Draw(lightingShader);
+
+        //Camino 3
+        camino = glm::translate(camino, glm::vec3(0.0f, 0.0f, 4.2f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(camino));
+        stone_path_lowpoly.Draw(lightingShader);
+
+        //Barril 1
+        barril = glm::translate(barril, glm::vec3(1.5f, 0.05f, 1.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(barril));
+        barrel.Draw(lightingShader);
+
+        //Barn
+        barril = glm::translate(barril, glm::vec3(18.0f, 0.0f, 18.0f));
+        model2 = barril;
+        barril = glm::rotate(barril, glm::radians(-90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        barril = glm::scale(barril, glm::vec3(0.5f, 0.5f, 0.5f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(barril));
+        barn.Draw(lightingShader);
+
+        //Arbol 5
+        model2 = glm::translate(model2, glm::vec3(9.0f, 0.0f, -2.0f));
+        model = model2;
+        model2 = glm::rotate(model2, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model2));
+        tree.Draw(lightingShader);
+
+        //Arbol 6
+        model = glm::translate(model, glm::vec3(-33.0f, 0.0f, 8.0f));
+        model = glm::rotate(model, glm::radians(-45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        tree.Draw(lightingShader);
+
         //glDrawArrays(GL_TRIANGLES, 0, 36);
         
 
@@ -259,11 +412,13 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
         model = glm::mat4(1.0f);
         model = glm::translate(model, lightPos + movelightPos);
-        model = glm::scale(model, glm::vec3(0.3f));
+        model = glm::scale(model, glm::vec3(1.0f));
         glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        sol.Draw(lampshader);
+        //glDrawArrays(GL_TRIANGLES, 0, 36);
         glBindVertexArray(0);
+
 
         // Swap the buffers
         glfwSwapBuffers(window);
